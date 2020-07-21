45da8f0fef9284e4782da51a3fedc12d3231fa33
Alexis Zugasti <alexiszugasti@gmail.com>
2020-07-21 01:15:53 -0500

    Project finished
    There could be some minor bugs needed in the future.

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,7 @@
-console.log('Wuddup');
-
 var control = 0;
-var stepText = ["Paso 1:", "Paso 2:", "Paso 3:", "Paso 4:", "Paso 5:", "¿Te queda espacio?"];
-var instText = ["Escoge tu base", "Escoge los vegetales que quieras", "Escoge la proteina", "Añade tus toppings", "Añade tu salsa", "Añade una entrada o bao"];
-var optsText = ["-Noodles de huevo <br>-Noodles verdes<br>-Noodles negros<br>-Noodles de arroz<br>-Noodles Veggie<br>-Arroz blanco al vapor<br>-Arroz de brocoli", "-Chicharo chino<br>-Champiñon<br>-Piña<br>-Edemames<br>-Brocoli<br>-Germen de soya<br>-Elote<br>-Pimiento<br>-Calabaza<br>-Zanahoria<br>-Cebolla<br>-Col Mix", "-Camarón<br>-Res<br>-Pollo<br>-Tofu<br>-Cerdo<br>-Surimi", "-Tocino<br>-Aguacate<br>-Huevo estrellado<br>-Cacahuate<br>-Ajo frito<br>-Cilantro", "-Ojo de tigre<br>-Miso Cilantro<br>-Curry Cacahuate<br>-Soya Cítrica<br>-Mango Chipotle<br>Sweet Chily<br>-Teriyaki<br>-Tamarindo", "-Bao buffalo wild<br>-Bao pollo crispy<br>-Bao pork belly<br>-Bao de camarón tempura<br>-Bao spicy beef<br>-Bao del mes<br>-Kushiage de queso<br>-Spring roll"];
+var stepText = ["Paso 1:", "Paso 2:", "Paso 3:", "Paso 4:", "Paso 5:", "¿Te queda espacio?", "¿Te queda espacio?", "¿De tomar?"];
+var instText = ["Escoge tu base", "Escoge los vegetales que quieras", "Escoge la proteina", "Añade tus toppings", "Añade tu salsa", "Añade una entrada", "¿Qué tal un Bao?", "Añade tu bebida"];
+var optsText = ["-Noodles de huevo <br>-Noodles verdes<br>-Noodles negros<br>-Noodles de arroz<br>-Noodles ntegrales<br>-Arroz blanco al vapor<br>-Arroz de brocoli", "-Chicharo chino<br>-Champiñon<br>-Piña<br>-Edemames<br>-Brocoli<br>-Germen de soya<br>-Elote<br>-Pimiento<br>-Calabaza<br>-Zanahoria<br>-Cebolla<br>-Col Mix", "-Camarón<br>-Res<br>-Pollo<br>-Tofu<br>-Cerdo<br>-Surimi", "-Tocino<br>-Aguacate<br>-Huevo estrellado<br>-Cacahuate<br>-Ajo frito<br>-Cilantro", "-Ojo de tigre<br>-Miso Cilantro<br>-Curry Cacahuate<br>-Soya Cítrica<br>-Mango Chipotle<br>Sweet Chily<br>-Teriyaki<br>-Tamarindo", "-Kushiage de queso<br>-Edamames<br>-Spring roll", "-Pork belly<br>-Camarón tempura<br>-Pollo crispy<br>-Spicy beef<br>-Buffalo wild<br>-El pollo szechuan<br>-Sweet bao<br>-PB&J bao<br>-Bao del mes", "-Agua natural<br>-Agua del día<br>-Refresco<br>-Té de la casa<br>-Galpi"];
     
 function upOne(){
     //Variables for the DOM are defined.
@@ -12,7 +10,7 @@ function upOne(){
     var third = document.getElementById("opts");
 
     control++;
-    if(control == 6){
+    if(control == 8){
         control = 0;
     }
     first.innerHTML = stepText[control];
@@ -28,7 +26,7 @@ function downOne(){
 
     control--;
     if(control == -1){
-        control = 5;
+        control = 7;
     }
     first.innerHTML = stepText[control];
     second.innerHTML = instText[control];
